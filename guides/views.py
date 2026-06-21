c04cf2a34045933e860d461492fcac629db36387
ehei123ai <ehei123@gmail.com>
2026-06-21 21:25:04 +0800

    Fixed Topbar & BottomNavBar

--- guides/views.py
+++ guides/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.db.models import Prefetch
 from django.views.generic import ListView
 from .models import GuideScenario, GuideQuestion
+#from django.contrib.auth.models import User
 
 class ScenarioQuestionListView(ListView):
     model = GuideScenario
@@ -26,6 +27,21 @@ class ScenarioQuestionListView(ListView):
                 )
             ).filter(is_active=True).order_by('-creation_date')
 
+'''
+    # Add this method to pass extra data to the template
+    def get_context_data(self, **kwargs):
+        # 1. Get the existing context data (contains your 'scenarios' list)
+        context = super().get_context_data(**kwargs)
+        
+        # 2. Add the currently logged-in user to the context
+        context['current_user'] = self.request.user
+        
+        # 3. (Optional) Example if you want to pull a specific target user by ID from the URL
+        # user_id = self.kwargs.get('user_id')
+        # context['target_user'] = User.objects.get(id=user_id)
+        
+        return context
+'''    
 
 # Create your views here.
 def guide(request):
